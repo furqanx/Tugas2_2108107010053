e286b3b9527865e6f06f42ea2a2156876f289e16
furqanx <alghifarifurqan03@gmail.com>
2023-03-06 10:14:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/javaandroid/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.javaandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView logo;
+    private ScrollView tampilanScroll;
+    private TextView teks;
+    private Button tombol;
+    private boolean isImage = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        logo = findViewById(R.id.logousk);
+        tampilanScroll = findViewById(R.id.scrollView2);
+        teks = findViewById(R.id.textView);
+
+        tombol = findViewById(R.id.button);
+        tombol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isImage) {
+                    logo.setImageResource(R.drawable.logo_usk_hitam_putih_png);
+                    isImage = false;
+                } else {
+                    logo.setImageResource(R.drawable.logoUSK);
+                    isImage = true;
+                }
+            }
+        });
+    }
+}
